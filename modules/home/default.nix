49c9e456da8c701fb1025e7e5c488159f9169f55
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2023-10-17 21:54:16 +0200

    gtk

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -29,6 +29,22 @@ in
     };
   };
 
+  gtk = {
+    enable = true;
+    theme = {
+      name = "arc-theme";
+      package = pkgs.arc-theme;
+    };
+    iconTheme = {
+      name = "Arc";
+      package = pkgs.arc-icon-theme;
+    };
+    cursorTheme = {
+      name = "Arc";
+      package = pkgs.arc-icon-theme;
+    };
+  };
+
   #https://github.com/tejing1/nixos-config/blob/master/homeConfigurations/tejing/encryption.nix
   programs.gpg.enable = true;
   services.gpg-agent = {
