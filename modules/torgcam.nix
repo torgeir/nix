8fdbe363e4ea322891f1403f85b4d9d71f19f70d
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2023-11-19 18:04:42 +0100

    more camera stuff, enough to take a snapshot

--- modules/torgcam.nix
+++ modules/torgcam.nix
@@ -5,8 +5,24 @@ nixpkgs.legacyPackages.x86_64-linux.pkgsCross.aarch64-multiplatform.nixos {
     "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-new-kernel-no-zfs-installer.nix"
     ./arm/sd-image.nix
     ({ lib, config, pkgs, ... }: {
+
+      # need a trusted user for deploy-rs.
+      nix.settings.trusted-users = [ "@wheel" ];
+
       boot = {
-        initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+        # https://discourse.nixos.org/t/load-automatically-kernel-module-and-deal-with-parameters/9200
+        initrd.availableKernelModules =
+          [ "xhci_pci" "usbhid" "usb_storage" "bcm2835-v4l2" ];
+        blacklistedKernelModules =
+          [ "i2c_bcm2708" "i2c_bcm2835" ]; # TODO bcm2835?
+        # TODO is this true
+        extraModprobeConfig = ''
+          options bcm2835-v4l2 max_video_width=3240 max_video_height=2464
+        '';
+
+        # v4l2-loopback? https://gist.github.com/TheSirC/93130f70cc280cdcdff89faf8d4e98ab
+
+        supportedFilesystems = [ "cifs" ];
 
         loader = {
           grub.enable = false;
@@ -16,7 +32,7 @@ nixpkgs.legacyPackages.x86_64-linux.pkgsCross.aarch64-multiplatform.nixos {
         kernelPackages = lib.mkForce (pkgs.linuxPackages_latest);
 
         # Avoids warning: mdadm: Neither MAILADDR nor PROGRAM has been set. This will cause the `mdmon` service to crash.
-        # See: https://github.com/NixOS/nixpkgs/issues/254807
+        # https://github.com/NixOS/nixpkgs/issues/254807
         swraid.enable = lib.mkForce false;
 
         # needed for deploy-rs
@@ -43,39 +59,41 @@ nixpkgs.legacyPackages.x86_64-linux.pkgsCross.aarch64-multiplatform.nixos {
         })
       ];
 
-      environment.systemPackages = with pkgs; [ neofetch vim ];
-
-      # ! Need a trusted user for deploy-rs.
-      nix.settings.trusted-users = [ "@wheel" ];
+      environment.systemPackages = with pkgs; [
+        neofetch
+        vim
+        htop
+        # https://www.tomshardware.com/how-to/use-raspberry-pi-camera-with-bullseye
+        libcamera # cam -h
+        # https://raspberrypi.stackexchange.com/questions/112743/v4l2-ctl-single-frame-capture-produces-image-with-green-ending
+        (v4l-utils.override { withGUI = false; }) # mismatched qt dependencies
+      ];
 
       sdImage.compressImage = false;
       sdImage.imageName = "${name}.img";
       sdImage.extraFirmwareConfig = {
-        # Give up VRAM for more Free System Memory
-        # - Disable camera which automatically reserves 128MB VRAM
-        # start_x = 0;
-        # - Reduce allocation of VRAM to 16MB minimum for non-rotated (32MB for rotated)
-        # gpu_mem = 16;
-
-        # torgeir
+        # camera setup
         gpu_mem = 128;
         start_file = "start_x.elf";
         fixup_file = "fixup_x.dat";
-        # torgeir fix "camera not enabled in build"
+        # fix "camera not enabled in build"
         cma_lwm = "";
         cma_hwm = "";
         cma_offline_start = "";
       };
 
-      # Make sure wifi works
+      # make sure wifi works
       hardware.enableRedistributableFirmware = lib.mkForce false;
       hardware.firmware = [ pkgs.raspberrypiWirelessFirmware ];
+
+      time.timeZone = "Europe/Oslo";
 
       services.openssh.enable = true;
       services.timesyncd.enable = true;
 
       system.stateVersion = "23.11";
 
+      # TODO disable ipv6
       networking = {
         hostName = name;
         wireless = {
@@ -92,7 +110,7 @@ nixpkgs.legacyPackages.x86_64-linux.pkgsCross.aarch64-multiplatform.nixos {
         isNormalUser = true;
         home = "/home/torgeir";
         description = "torgeir";
-        extraGroups = [ "wheel" "networkmanager" ];
+        extraGroups = [ "wheel" "networkmanager" "video" ];
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIITJ5UIW0lXbeFfyOrdCXAfBtZsq/NycSzIADDZDi3TL"
         ];
@@ -104,6 +122,29 @@ nixpkgs.legacyPackages.x86_64-linux.pkgsCross.aarch64-multiplatform.nixos {
       };
 
       # services.getty.autologinUser = lib.mkForce "torgeir";
+
+      users.users.motion = {
+        group = "motion";
+        description = "motion camera user";
+        uid = 510;
+        extraGroups = [ "video" ];
+      };
+      users.groups.motion.gid = 510;
+
+      systemd.mounts = map (mount: {
+        description = "Mount ${mount}";
+        what = "//fileserver/${mount}";
+        where = "/run/mount/${mount}";
+        type = "cifs";
+        options =
+          "_netdev,username=,password=,uid=1000,gid=1000,iocharset=utf8,rw,vers=3.0";
+      }) [ "cam" ];
+
+      systemd.automounts = map (mount: {
+        description = "Automount /${mount}";
+        where = "/run/mount/${mount}";
+        wantedBy = [ "multi-user.target" ];
+      }) [ "cam" ];
 
     })
 
