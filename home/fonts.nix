beae0bbe8f33f0d318ae4da1e7e9b3858785d584
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2023-12-11 20:48:26 +0100

    note on patching fonts

--- home/fonts.nix
+++ home/fonts.nix
@@ -5,4 +5,8 @@
   # discover fonts installed through home.packages and nix-env
   fonts.fontconfig.enable = true;
 
+  # TODO patch fonts yourself
+  # https://codeberg.org/municorn/iosevka-muse/src/commit/ca362ea18ef57eef395a96b9f9c9cb06d1fef3f2/flake.nix#L24
+  # https://dee.underscore.world/blog/home-manager-fonts/
+
 }
