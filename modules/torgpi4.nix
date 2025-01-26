04289db5cb6a333dd3e34c43c09a78f0dfc9e872
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2025-01-26 06:48:50 +0100

    Revert "wip"
    This reverts commit fe6e8f74d3dabcccd2f9b92cbd186b14e29e6b09.

--- modules/torgpi4.nix
+++ modules/torgpi4.nix
@@ -1,169 +1,111 @@
-{ inputs, name, pkgs, lib, stdenv, fetchFromGitHub, ... }:
 
-{
-  # make nix crosscompile, from an x86 linux box, to an arm image
-  nixpkgs.buildPlatform.system = "x86_64-linux";
-  nixpkgs.hostPlatform.system = "aarch64-linux";
+{ name, nixpkgs }:
 
-  nixpkgs.config.allowUnfree = true; # reaper
+nixpkgs.legacyPackages.x86_64-linux.pkgsCross.aarch64-multiplatform.nixos {
+  imports = [
 
-  # need a trusted user for deploy-rs.
-  nix.settings.trusted-users = [ "@wheel" ];
+    "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-new-kernel-no-zfs-installer.nix"
 
-  nix.settings.substituters = [ "https://cache.nixos.org" ];
+    ({ lib, config, pkgs, ... }: {
 
-  boot = {
-    kernelParams = [ "console=ttyAMA0,115200n8" "console=ttyS0,115200n8" ];
-    kernelModules = [ "i2c-dev" ];
-    initrd.availableKernelModules = pkgs.lib.mkForce [ "mmc_block" ];
+      # need a trusted user for deploy-rs.
+      nix.settings.trusted-users = [ "@wheel" ];
 
-    # v4l2-loopback? https://gist.github.com/TheSirC/93130f70cc280cdcdff89faf8d4e98ab
+      boot = {
+        kernelParams = [ "console=ttyAMA0,115200n8" "console=ttyS0,115200n8" ];
+        kernelModules = [ "i2c-dev" ];
+        initrd.availableKernelModules = pkgs.lib.mkForce [ "mmc_block" ];
 
-    # supportedFilesystems = [ "cifs" ];
+        # v4l2-loopback? https://gist.github.com/TheSirC/93130f70cc280cdcdff89faf8d4e98ab
 
-    loader = {
-      grub.enable = false;
-      generic-extlinux-compatible.enable = true;
-    };
+        # supportedFilesystems = [ "cifs" ];
 
-    kernelPackages = lib.mkForce (pkgs.linuxPackages_latest);
+        loader = {
+          grub.enable = false;
+          generic-extlinux-compatible.enable = true;
+        };
 
-    # Avoids warning: mdadm: Neither MAILADDR nor PROGRAM has been set.
-    # This will cause the `mdmon` service to crash.
-    # https://github.com/NixOS/nixpkgs/issues/254807
-    # swraid.enable = lib.mkForce false;
+        kernelPackages = lib.mkForce (pkgs.linuxPackages_latest);
 
-    # needed for deploy-rs
-    binfmt.emulatedSystems = [ "x86_64-linux" ];
-  };
+        # Avoids warning: mdadm: Neither MAILADDR nor PROGRAM has been set.
+        # This will cause the `mdmon` service to crash.
+        # https://github.com/NixOS/nixpkgs/issues/254807
+        # swraid.enable = lib.mkForce false;
 
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD"; # name of sd card when written
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
-  };
+        # needed for deploy-rs
+        binfmt.emulatedSystems = [ "x86_64-linux" ];
+      };
 
-  zramSwap = {
-    enable = true;
-    algorithm = "zstd";
-  };
+      fileSystems = {
+        "/" = {
+          device = "/dev/disk/by-label/NIXOS_SD"; # name of sd card when written
+          fsType = "ext4";
+          options = [ "noatime" ];
+        };
+      };
 
-  environment.systemPackages = with pkgs; [
-    # neofetch
-    # vim
-    # htop
+      zramSwap = {
+        enable = true;
+        algorithm = "zstd";
+      };
 
-    # https://man.archlinux.org/man/calf.7.en
-    # https://github.com/andyb2000/rivendell-tools/blob/8902fec1027be2e4f7c222b4ea5afcdd837ac613/calf_forever.sh
-    jalv
-    # guitarix
-    # neural-amp-modeler-lv2
-    # jack2
-    # qjackctl
-  ];
+      nixpkgs.overlays = [
+        (final: super: {
+          makeModulesClosure = x:
+            super.makeModulesClosure (x // { allowMissing = true; });
+        })
+      ];
 
-  sdImage.compressImage = false;
-  sdImage.imageName = "${name}.img";
+      environment.systemPackages = with pkgs; [
+        neofetch
+        vim
+        htop
+      ];
 
-  # make sure wifi works
-  hardware.enableRedistributableFirmware = lib.mkForce false;
-  hardware.firmware = [ pkgs.raspberrypiWirelessFirmware ];
+      sdImage.compressImage = false;
+      sdImage.imageName = "${name}.img";
 
-  time.timeZone = "Europe/Oslo";
+      # make sure wifi works
+      hardware.enableRedistributableFirmware = lib.mkForce false;
+      hardware.firmware = [ pkgs.raspberrypiWirelessFirmware ];
 
-  services.openssh.enable = true;
-  services.timesyncd.enable = true;
+      time.timeZone = "Europe/Oslo";
 
-  system.stateVersion = "24.11";
+      services.openssh.enable = true;
+      services.timesyncd.enable = true;
 
-  networking = {
-    hostName = name;
-    wireless = {
-      enable = true;
-      networks."znort-guest".psk = "";
-      interfaces = [ "wlan0" ];
-    };
-    interfaces."wlan0".useDHCP = true;
-  };
+      system.stateVersion = "24.11";
 
-  # users.users.root.openssh.authorizedKeys.keys = [ ];
+      networking = {
+        hostName = name;
+        wireless = {
+          enable = true;
+          networks."znort-guest".psk = "";
+          interfaces = [ "wlan0" ];
+        };
+        interfaces."wlan0".useDHCP = true;
+      };
 
-  nixpkgs.overlays = [
-    (final: prev: {
-      makeModulesClosure = x:
-        prev.makeModulesClosure (x // { allowMissing = true; });
+      # users.users.root.openssh.authorizedKeys.keys = [ ];
+
+      users.users.nam = {
+        isNormalUser = true;
+        home = "/home/nam";
+        description = "nam";
+        extraGroups = [ "wheel" "networkmanager" "video" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIITJ5UIW0lXbeFfyOrdCXAfBtZsq/NycSzIADDZDi3TL"
+        ];
+      };
+
+      security.sudo = {
+        enable = true;
+        wheelNeedsPassword = false;
+      };
+
+      services.getty.autologinUser = lib.mkForce "nam";
+
     })
 
-    (final: prev:
-      let inherit (prev) callPackage;
-      in {
-        neural-amp-modeler-lv2 = callPackage ../pkgs/neural-amp-modeler-lv2 { };
-      })
   ];
-
-  users.users.nam = {
-    isNormalUser = true;
-    home = "/home/nam";
-    description = "nam";
-    extraGroups = [ "wheel" "networkmanager" "video" "jackaudio"];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIITJ5UIW0lXbeFfyOrdCXAfBtZsq/NycSzIADDZDi3TL"
-    ];
-  };
-
-  security.sudo = {
-    enable = true;
-    wheelNeedsPassword = false;
-  };
-
-  services.getty.autologinUser = lib.mkForce "nam";
-
-
-  services.xserver = {
-    enable = true;
-    autorun = true;
-    displayManager = {
-      lightdm.enable = true;
-    };
-    windowManager = {
-      dwm.enable = true;
-    };
-  };
-  services.displayManager.defaultSession = "none+dwm";
-  # services.displayManager.autoLogin = true;
-
-
-  ##
-  ## nam sound specific setup
-  ##
-
-  # security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = true;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pulse.enable = true;
-  # };
-
-  # https://nixos.wiki/wiki/JACK
-
-  # services.jack = {
-  #   jackd.enable = true;
-  #   # support ALSA only programs via ALSA JACK PCM plugin
-  #   alsa.enable = false;
-  #   # support ALSA only programs via loopback device (supports programs like Steam)
-  #   loopback = {
-  #     enable = true;
-  #     # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
-  #     #dmixConfig = ''
-  #     #  period_size 2048
-  #     #'';
-  #   };
-  # };
-
-
-  # TODO LV2 path home sessionVariable
-
 }
