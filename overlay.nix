aaf0cba7e5315e727168ea61cdc3e323eacc8495
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2025-04-11 20:40:07 +0200

    remove neural-amp-modeler-lv2, use nix one instead

--- overlay.nix
+++ overlay.nix
@@ -3,8 +3,6 @@ final: prev:
 
 let inherit (prev) lib callPackage;
 in {
-  # remember to add these files to git for nix to discover them
-  neural-amp-modeler-lv2 = callPackage ./pkgs/neural-amp-modeler-lv2 { };
 
   # opentrack with neural-net tracker support
   opentrack = prev.opentrack.overrideAttrs (old: rec {
