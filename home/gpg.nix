3d22896302e660487082fd9f13f8076892f29b62
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2025-07-13 19:43:43 +0200

    flake update dotfiles, nix-ha, gpg 1p

--- home/gpg.nix
+++ home/gpg.nix
@@ -1,6 +1,33 @@
 { config, lib, inputs, pkgs, ... }:
 
-{
+let dotfiles = inputs.dotfiles;
+    pinentry = pkgs.writeShellScriptBin "gpg-1p-pinentry" ''
+exec 3>&1 4>&2
+echo "OK Pleased to meet you"
+while IFS= read -r line; do
+  case "$line" in
+    "GETPIN")
+      if PASSPHRASE=$(/run/wrappers/bin/op item get keybase.io --format json | /etc/profiles/per-user/torgeir/bin/jq  -j '.fields[] | select(.id == "password") | .value' 2>/dev/null); then
+        echo "D $PASSPHRASE"
+        echo "OK"
+      else
+        echo "ERR 83886179 Failed to retrieve passphrase <Pinentry>"
+      fi
+      ;;
+    "PKDECRYPT")
+      echo "OK"
+      ;;
+    "BYE")
+      echo "OK closing connection"
+      exit 0
+      ;;
+    *)
+      echo "OK"
+      ;;
+  esac
+done
+'';
+in {
 
   #https://github.com/tejing1/nixos-config/blob/master/homeConfigurations/tejing/encryption.nix
   # https://freerangebits.com/posts/2023/12/gnupg-broke-emacs/
@@ -17,8 +44,8 @@
     maxCacheTtlSsh = 28800;
     defaultCacheTtlSsh = 28800;
     extraConfig = ''
-      allow-preset-passphrase
-      pinentry-program ${pkgs.pinentry-qt}/bin/pinentry-qt
+      pinentry-program "${lib.getExe pinentry}"
     '';
   };
+
 }
