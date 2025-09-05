41a57206d99a220589ecf0dcb28d0ced00d23e84
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2025-09-05 10:07:40 +0200

    openrgb.nix

---
+++ modules/openrgb.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  boot.kernelModules = [ "i2c-dev" "i2c-piix4" ];
+  services.udev.packages = [ pkgs.openrgb ];
+  environment.systemPackages = [ pkgs.openrgb ];
+
+  # environment.etc."openrgb/default-profile.orp".source = ./Lights-off.orp;
+  # systemd.services.openrgb = {
+  #   wantedBy = [ "default.target" ];
+  #   script = ''
+  #     ${pkgs.openrgb}/bin/openrgb --noautoconnect --profile /etc/openrgb/default-profile.orp
+  #   '';
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     RemainAfterExit = true;
+  #   };
+  #   restartTriggers = [ "/etc/openrgb/default-profile.orp" ];
+  # };
+
+}
