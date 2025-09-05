eed4cdb99d5b57c6234ed492d3b085ada17f3dfc
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2025-09-05 10:08:05 +0200

    pip on own screen

--- home/default.nix
+++ home/default.nix
@@ -71,7 +71,8 @@ in {
       # Lookup windows: https://gist.github.com/dshoreman/278091a17c08e30c46c7e7988b7c2f7d
       # What goes where
 
-      for_window [title="Picture-in-Picture"] floating enable; sticky enable; border none; move window to workspace number 9; resize set height 405px; resize set width 720px; move position 600px 2000px;
+      for_window [title="Picture-in-Picture"] floating disable; sticky enable; move window to workspace number 9; resize set height 1600px;
+      #border none; resize set width 720px; move position 600px 2000px;
 
       assign [app_id="emacs"] 2
       assign [app_id="org.rncbc.qpwgraph"] 4
