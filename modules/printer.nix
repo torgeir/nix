3cba33a9ea86abdc7c5af7c3847473241bd2ba2f
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2025-10-03 18:52:51 +0200

    declarative printer setup

--- modules/printer.nix
+++ modules/printer.nix
@@ -2,57 +2,32 @@
 
   # https://nixos.wiki/wiki/Printing
 
-  # canon iX6500 printer
+  # Canon iX6500 printer
 
-  environment.systemPackages = with pkgs; [
-    cups
-  ];
+  environment.systemPackages = with pkgs; [ cups ];
 
   services.printing =  {
-
    logLevel = "warning";
-
    enable = true;
-   drivers = with pkgs; [
-    gutenprint
-    # not needed
-    # canon-cups-ufr2
-    # cnijfilter2
-   ];
-
-   # TODO not needed?
-   # extraConf = ''
-   #   <Location />
-   #     Order allow,deny
-   #     Allow @LOCAL
-   #   </Location>
-   #   <Location /admin>
-   #     Order allow,deny
-   #     Allow @LOCAL
-   #   </Location>
-   #   <Location /admin/conf>
-   #     AuthType Default
-   #     Require user @SYSTEM
-   #     Order allow,deny
-   #     Allow @LOCAL
-   #   </Location>
-   #  '';
-
+   drivers = with pkgs; [ gutenprint ];
   };
 
-  # TODO untested
-  #hardware.printers.ensurePrinters = [
-  #  {
-  #    name = "canonix6500";
-  #    location = "Kontoret";
-  #    deviceUri = "usb://Canon/iX6500%20series?serial=007814";
-  #    model = "/etc/cups/ppd/Canon_iX6500_series.ppd";
-  #    ppdOptions = {
-  #      PageSize = "A4";
-  #    };
-  #  }
-  #];
+  hardware.printers.ensurePrinters = [
+    {
+      # pick one
+      name = "canonix6500";
+      location = "Kontoret";
+      deviceUri = "usb://Canon/iX6500%20series?serial=007814";
+      # lpinfo -m | grep iX6500
+      model = "gutenprint.5.3://bjc-PIXMA-iX6500/expert";
+      ppdOptions = {
+        PageSize = "A4";
+      };
+    }
+  ];
 
+  # or manually:
+  #
   # - visit https://localhost:631/admin
   # - add printer, driver: Canon iX6500 series - CUPS+Gutenprint v5.3.5 (color)
   # - admin, add allowed users
