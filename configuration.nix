f35f4739e8fb76d1f01e526d99215d0ec00877ab
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2025-11-01 09:13:27 +0100

    force logitech c920 setting ALL THE TIME

--- configuration.nix
+++ configuration.nix
@@ -274,24 +274,35 @@ in {
     # head tracking
     opentrack
     (writeScriptBin "opentrack-xwayland" ''
+      #!/usr/bin/env bash
       # get the logitech c920s (with IR filter removed) ready for opentrack with Input PointTracker 1.1.
-      v4l2-ctl -d /dev/video0 -c brightness=0
-      v4l2-ctl -d /dev/video0 -c contrast=0
-      v4l2-ctl -d /dev/video0 -c saturation=0
-      v4l2-ctl -d /dev/video0 -c sharpness=0
-      v4l2-ctl -d /dev/video0 -c white_balance_automatic=0
-      v4l2-ctl -d /dev/video0 -c backlight_compensation=0
-      v4l2-ctl -d /dev/video0 -c exposure_dynamic_framerate=0
-      v4l2-ctl -d /dev/video0 -c auto_exposure=1 # manual mode
-      v4l2-ctl -d /dev/video0 -c pan_absolute=0
-      v4l2-ctl -d /dev/video0 -c tilt_absolute=0
-      v4l2-ctl -d /dev/video0 -c focus_automatic_continuous=0
-      v4l2-ctl -d /dev/video0 -c focus_absolute=40
-      v4l2-ctl -d /dev/video0 -c zoom_absolute=195
-      v4l2-ctl -d /dev/video0 -c gain=39 # must be <60 for logitech c920s to manage ~60fps
-      v4l2-ctl -d /dev/video0 -c white_balance_temperature=2000
-      v4l2-ctl -d /dev/video0 -c exposure_time_absolute=9
-      env -u WAYLAND_DISPLAY opentrack
+      #d=''${1:-/dev/video0}
+      # get correct /dev/videoX from v4l2-ctl
+      d=$(v4l2-ctl --list-devices | grep -A1 "HD Pro Webcam" | tail -1 | tr -d '\t')
+      echo video device: $d
+      env -u WAYLAND_DISPLAY opentrack &
+      # must use <60 gain for logitech c920s to manage ~60fps;
+      # why u say? it needs to happen a little later. i have no idea.
+      { while pgrep -x "opentrack" > /dev/null; do
+        v4l2-ctl -d $d -c brightness=0;
+        v4l2-ctl -d $d -c contrast=0;
+        v4l2-ctl -d $d -c saturation=0;
+        v4l2-ctl -d $d -c sharpness=0;
+        v4l2-ctl -d $d -c white_balance_automatic=0;
+        v4l2-ctl -d $d -c backlight_compensation=0;
+        v4l2-ctl -d $d -c exposure_dynamic_framerate=0;
+        v4l2-ctl -d $d -c auto_exposure=1;
+        v4l2-ctl -d $d -c pan_absolute=0;
+        v4l2-ctl -d $d -c tilt_absolute=0;
+        v4l2-ctl -d $d -c focus_automatic_continuous=0;
+        v4l2-ctl -d $d -c focus_absolute=40;
+        v4l2-ctl -d $d -c zoom_absolute=195;
+        v4l2-ctl -d $d -c gain=39;
+        v4l2-ctl -d $d -c white_balance_temperature=2000;
+        v4l2-ctl -d $d -c exposure_time_absolute=9;
+        sleep 5;
+        done; } &
+      wait
     '')
 
     # webcam support, v4l2-ctl --list-devices
