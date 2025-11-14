f203c7f046a505fa56230fc2b9aab2be61999de7
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2025-11-14 10:14:08 +0100

    acme/lego/cf change or never tried this since DNS over TLS?

--- modules/acme.nix
+++ modules/acme.nix
@@ -6,11 +6,13 @@
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "torgeir.thoresen@gmail.com";
   security.acme.certs."wa.gd" = {
-    domain = "wa.gd";
-    extraDomainNames = [ "*.wa.gd" ];
+    domain = "*.wa.gd";
+
+    # TODO does this setup work with dns over 853? might need to turn of that NAT rule in the router
     dnsProvider = "cloudflare";
     dnsResolver = "1.1.1.1:53"; # nescessary
-    dnsPropagationCheck = true;
+    # dnsPropagationCheck = true;
+    dnsPropagationCheck = false;
 
     # secret needs needs CLOUDFLARE_DNS_API_TOKEN=<token>,
     # with cloudflare scopes Zone:Zone:read and Zone:DNS:edit
