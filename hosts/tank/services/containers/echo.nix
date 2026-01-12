84e34ca4dc0f33ccc37effb7832a01a2cf9836fb
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2026-01-12 20:33:12 +0100

    dummy service in podman network web

--- hosts/tank/services/containers/echo.nix
+++ hosts/tank/services/containers/echo.nix
@@ -8,6 +8,13 @@
 {
   virtualisation.oci-containers.containers."hello" = {
     image = "traefik/whoami";
+    extraOptions = ["--network=web"];
     ports = [ "8080:80" ];
   };
+
+  system.activationScripts.createPodmanNetworkWeb = lib.mkAfter ''
+    if ! /run/current-system/sw/bin/podman network exists web; then
+      /run/current-system/sw/bin/podman network create web
+    fi
+  '';
 }
