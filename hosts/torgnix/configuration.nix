7d110b5492ed15f410aa214e6ce4a597c7695ae9
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2026-01-17 11:30:02 +0100

    fix thunar plugins moved
    evaluation warning: ‘xfce.thunar-archive-plugin’ was moved to top-level. Please use ‘pkgs.thunar-archive-plugin’ directly
    evaluation warning: ‘xfce.thunar-volman’ was moved to top-level. Please use ‘pkgs.thunar-volman’ directly

--- hosts/torgnix/configuration.nix
+++ hosts/torgnix/configuration.nix
@@ -234,7 +234,7 @@ in
   services.tailscale.enable = true;
 
   programs.thunar.enable = true;
-  programs.thunar.plugins = with pkgs.xfce; [
+  programs.thunar.plugins = with pkgs; [
     thunar-archive-plugin
     thunar-volman
   ];
