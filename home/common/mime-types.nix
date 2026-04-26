0ac65d52665ffafe92374ae6dbab1ebcda4afde2
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2026-04-26 14:13:26 +0200

    emacsclient needs server folder

--- home/common/mime-types.nix
+++ home/common/mime-types.nix
@@ -16,7 +16,7 @@
   };
   xdg.desktopEntries.emacsclient-pdf = {
     name = "Emacs Client";
-    exec = "emacsclient -c %u";
+    exec = ''emacsclient --socket-name ${config.home.homeDirectory}/.emacs.d/server/server -a "" -n -q %u'';
     mimeType = [ "application/pdf" ];
   };
 }
