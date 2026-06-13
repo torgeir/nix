1bde07994c919dbe3d462c89bb91e22ac0e1d9ff
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2026-06-13 18:42:18 +0200

    no ollama on tank

--- home/torgeir/tank.nix
+++ home/torgeir/tank.nix
@@ -11,12 +11,30 @@
 
   imports = [
     (inputs.nix-home-manager + "/modules")
-    ../common/ollama.nix
+    # ../common/ollama.nix
   ];
+
+  home.packages = with pkgs; [
+    pi-coding-agent
+    inxi
+  ];
+
+  # home.sessionVariables = {
+  #   OLLAMA_MODELS = "/fast/shared/apps/ollama/models";
+  # };
+  # services.ollama = {
+  #   environmentVariables = {
+  #     OLLAMA_MODELS = "/fast/shared/apps/ollama/models";
+  #   };
+  # };
 
   programs.t-git.enable = true;
   programs.t-nvim.enable = true;
+
   programs.t-shell-tooling.enable = true;
+  # make session vars load
+  programs.zsh.enable = true;
+
   programs.t-zoxide.enable = true;
   programs.t-tmux.enable = true;
 
