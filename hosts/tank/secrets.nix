b5b520b7d04e7d5ef1c022667a6ffe3263f156d9
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2026-06-15 20:31:30 +0200

    rekey torgnix, too

--- hosts/tank/secrets.nix
+++ hosts/tank/secrets.nix
@@ -7,14 +7,6 @@
       smb-torgeir-password = {
         file = ../../secrets/smb-torgeir-password.age;
       };
-      secret1 = {
-        file = ../../secrets/secret1.age;
-        # group = "..";
-        # owner = "..";
-        # mode "0440";
-        # path = "/home/torgeir/.secret1"
-      };
-
       immich-db-password = {
         file = ../../secrets/immich-db-password.age;
       };
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,57 +1,55 @@
 let
   tank = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICIEQt9BNUV7U13ekLERFnHvf2FIKWx8zVKq8TP28a/H root@tank";
   torgeir = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/yYpuSnWCBRKX/3bY1csXcNMgwVqyS5UArfBvXUkhk torgeir@torgnix";
+  torgnix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJM6CNNRtA1DnAtFPLOSmKdKAE8vlFfa+QJXcfa28lwt torgeir@torgnix";
+  admins = [ torgeir ];
   hosts = [
     tank
-    torgeir
+    torgnix
   ];
 in
 {
-  # nix run github:ryantm/agenix -- -e secret1.age
-  # nix run github:ryantm/agenix -- --rekey -e secret1.age
-  # to rekey you must have private key to decrypt it first
-  "secret1.age".publicKeys = hosts;
-
-  # nix run github:ryantm/agenix -- -e smb-maja-password.age
-  "smb-maja-password.age".publicKeys = hosts;
-  # nix run github:ryantm/agenix -- -e smb-torgeir-password.age
-  "smb-torgeir-password.age".publicKeys = hosts;
   #nix run github:ryantm/agenix -- -e smb-torgeir-credentials.age
   #     username: <username>
   #     password: <password>
-  "smb-torgeir-credentials.age".publicKeys = hosts;
+  "smb-torgeir-credentials.age".publicKeys = admins ++ [ torgnix ];
+
+  # nix run github:ryantm/agenix -- -e torgnix-acme-cf.age
+  "torgnix-acme-cf.age".publicKeys = admins ++ [ torgnix ]; # FIXME outdated
+
+  # nix run github:ryantm/agenix -- -e smb-maja-password.age
+  "smb-maja-password.age".publicKeys = admins ++ [ tank ];
+  # nix run github:ryantm/agenix -- -e smb-torgeir-password.age
+  "smb-torgeir-password.age".publicKeys = admins ++ [ tank ];
 
   # immich-app/immich-server
   # nix run github:ryantm/agenix -- -e immich-db-password.age
-  "immich-db-password.age".publicKeys = hosts;
+  "immich-db-password.age".publicKeys = admins ++ [ tank ];
   # immich-app/postgres
   # nix run github:ryantm/agenix -- -e immich-postgres.age
-  "immich-postgres.age".publicKeys = hosts;
+  "immich-postgres.age".publicKeys = admins ++ [ tank ];
 
   # CLOUDFLARE_DNS_API_TOKEN=<token>
   # nix run github:ryantm/agenix -- -e tank-acme-cf.age
-  "tank-acme-cf.age".publicKeys = hosts;
-
-  # nix run github:ryantm/agenix -- -e torgnix-acme-cf.age
-  "torgnix-acme-cf.age".publicKeys = [ torgeir ];
+  "tank-acme-cf.age".publicKeys = admins ++ [ tank ]; # FIXME outdated
 
   # nix run github:ryantm/agenix -- -e freshrss.age
-  "freshrss.age".publicKeys = hosts;
+  "freshrss.age".publicKeys = admins ++ [ tank ];
 
   # wallabag app env (SYMFONY__ENV__DATABASE_PASSWORD, SYMFONY__ENV__SECRET)
   # nix run github:ryantm/agenix -- -e wallabag-env.age
-  "wallabag-env.age".publicKeys = hosts;
+  "wallabag-env.age".publicKeys = admins ++ [ tank ];
   # wallabag postgres
   # nix run github:ryantm/agenix -- -e wallabag-postgres.age
-  "wallabag-postgres.age".publicKeys = hosts;
+  "wallabag-postgres.age".publicKeys = admins ++ [ tank ];
 
   # nix run github:ryantm/agenix -- -e scrobble.age
-  "scrobble.age".publicKeys = hosts;
+  "scrobble.age".publicKeys = admins ++ [ tank ];
 
   # nix-shell -p apacheHttpd --run 'htpasswd -c webdav-htpasswd <user>'
   # cat webdav-htpasswd | wl-copy; rm webdav-htpasswd;
   # paste it here:
   # nix run github:ryantm/agenix -- -e webdav-htpasswd.age
-  "webdav-htpasswd.age".publicKeys = hosts;
+  "webdav-htpasswd.age".publicKeys = admins ++ [ tank ];
 
 }
