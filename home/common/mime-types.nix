c59f49aaa5b53063c2476253ed7a9a8db97ea6c2
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2026-09-08 11:13:21 +0200

    mime types

--- home/common/mime-types.nix
+++ home/common/mime-types.nix
@@ -10,13 +10,45 @@
   xdg.enable = true;
   xdg.mimeApps.enable = true;
 
-  # open pdfs in emacs
+  # force sane defaults instead, not to let wine mess up
+  xdg.mimeApps.defaultApplications = {
+    "application/pdf" = "emacsclient-pdf.desktop";
+
+    "text/plain" = "emacsclient.desktop";
+    "text/x-log" = "emacsclient.desktop";
+    "application/x-shellscript" = "emacsclient.desktop";
+
+    "image/png" = "swayimg.desktop";
+    "image/jpeg" = "swayimg.desktop";
+    "image/jpg" = "swayimg.desktop";
+    "image/gif" = "swayimg.desktop";
+    "image/bmp" = "swayimg.desktop";
+    "image/webp" = "swayimg.desktop";
+    "image/tiff" = "swayimg.desktop";
+    "image/svg+xml" = "swayimg.desktop";
+    "image/x-portable-pixmap" = "swayimg.desktop";
+    "image/x-portable-bitmap" = "swayimg.desktop";
+    "image/x-portable-anymap" = "swayimg.desktop";
+    "image/x-portable-graymap" = "swayimg.desktop";
+  };
+
   xdg.mimeApps.associations.added = {
     "application/pdf" = "emacsclient-pdf.desktop";
   };
+
   xdg.desktopEntries.emacsclient-pdf = {
     name = "Emacs Client";
     exec = ''emacsclient --socket-name ${config.home.homeDirectory}/.emacs.d/server/server -a "" -n -q %u'';
     mimeType = [ "application/pdf" ];
+  };
+
+  xdg.desktopEntries.emacsclient = {
+    name = "Emacs Client";
+    exec = ''emacsclient --socket-name ${config.home.homeDirectory}/.emacs.d/server/server -a "" -n -q %u'';
+    mimeType = [
+      "text/plain"
+      "text/x-log"
+      "application/x-shellscript"
+    ];
   };
 }
